a689e076809ac65379aa25502d0f48bb90110abf
leo <leojimdev@gmail.com>
2026-08-04 18:17:33 -0400

    stow folder structure

--- old/arch-omarchy/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-#     # Commands to run in interactive sessions can go here
-    if test (tput cols) -ge 100
-        fastfetch
-    end
-end
--- old/cachyos-omarchy/nvim/lua/plugins/rootdir.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "ahmedkhalf/project.nvim",
-    opts = {},
-    config = function(_, opts)
-      require("project_nvim").setup(opts)
-    end,
-  },
-}
